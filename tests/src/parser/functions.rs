37a90baec2e3f5098a7f9df3c74171dbfefeb63b
Daniel Solarte <danielsolartech@hotmail.com>
2020-08-23 10:09:09 -0500

    feature(tests): Added more functions tests

--- tests/src/parser/functions.rs
+++ tests/src/parser/functions.rs
@@ -20,20 +20,197 @@ fn test_function(value: &str, expect: Box<Statements>) {
 }
 
 #[cfg(test)]
+fn get_function() -> Function {
+  Statement::from_token(Token::from_value(String::from("function"), 1, 1))
+}
+
+#[cfg(test)]
 fn get_box(function: Function) -> Box<Statements> {
   Box::new(Statements::FUNCTION(function))
 }
 
+#[cfg(test)]
+fn empty_function() -> Box<Statements> {
+  let mut function: Function = get_function();
+
+  function.name = Identifier::new_box_from_token(lexer::get_identifier("empty", 1, 10));
+  function.body = Block::new_box_from_token(Token::from_value(String::from("{"), 1, 18));
+
+  get_box(function)
+}
+
+#[cfg(test)]
+fn type_function(data_type: &str) -> Box<Statements> {
+  let mut function: Function = get_function();
+
+  function.name = Identifier::new_box_from_token(lexer::get_identifier("type", 1, 10));
+  function.data_type = Token::from_value(data_type.to_string(), 1, 18);
+  function.body = Block::new_box_from_token(Token::from_value(String::from("{"), 1, data_type.len() + 19));
+
+  get_box(function)
+}
+
+#[cfg(test)]
+fn arguments_function() -> Box<Statements> {
+  let mut function: Function = get_function();
+
+  function.name = Identifier::new_box_from_token(lexer::get_identifier("add", 1, 10));
+
+  let mut x_arg = Argument::from_token(lexer::get_identifier("x", 1, 14));
+  x_arg.data_type = Token::from_value(String::from("number"), 1, 17);
+
+  function.arguments.push(Box::new(Expressions::ARGUMENT(x_arg)));
+
+  let mut y_arg = Argument::from_token(lexer::get_identifier("y", 1, 25));
+  y_arg.data_type = Token::from_value(String::from("number"), 1, 28);
+
+  function.arguments.push(Box::new(Expressions::ARGUMENT(y_arg)));
+
+  function.data_type = Token::from_value(String::from("number"), 1, 37);
+  function.body = Block::new_box_from_token(Token::from_value(String::from("{"), 1, 44));
+
+  get_box(function)
+}
+
+#[cfg(test)]
+fn get_variable(name: &str) -> Variable {
+  let mut variable: Variable = Statement::from_token(Token::from_value(String::from("let"), 1, 1));
+
+  variable.name = Identifier::new_box_from_token(lexer::get_identifier(name, 1, 5));
+  variable.data_type = Token::from_value(String::from("void"), 0, 0);
+
+  variable
+}
+
+#[cfg(test)]
+fn empty_anonymous_function_1() -> Box<Statements> {
+  let mut variable: Variable = get_variable("empty");
+
+  let mut function: AnonymousFunction = Expression::from_token(Token::from_value(String::from("function"), 1, 13));
+
+  function.body = Block::new_box_from_token(Token::from_value(String::from("{"), 1, 25));
+
+  variable.value = Some(Box::new(Expressions::ANONYMOUSFUNCTION(function)));
+
+  Box::new(Statements::VARIABLE(variable))
+}
+
+#[cfg(test)]
+fn empty_anonymous_function_2() -> Box<Statements> {
+  let mut variable: Variable = get_variable("empty");
+
+  let mut function: AnonymousFunction = Expression::from_token(Token::from_value(String::from("("), 1, 13));
+
+  function.body = Block::new_box_from_token(Token::from_value(String::from("{"), 1, 19));
+
+  variable.value = Some(Box::new(Expressions::ANONYMOUSFUNCTION(function)));
+
+  Box::new(Statements::VARIABLE(variable))
+}
+
+#[cfg(test)]
+fn type_anonymous_function_1(data_type: &str) -> Box<Statements> {
+  let mut variable: Variable = get_variable("type");
+
+  let mut function: AnonymousFunction = Expression::from_token(Token::from_value(String::from("function"), 1, 12));
+
+  function.data_type = Token::from_value(data_type.to_string(), 1, 25);
+
+  function.body = Block::new_box_from_token(Token::from_value(String::from("{"), 1, data_type.len() + 26));
+
+  variable.value = Some(Box::new(Expressions::ANONYMOUSFUNCTION(function)));
+
+  Box::new(Statements::VARIABLE(variable))
+}
+
+#[cfg(test)]
+fn type_anonymous_function_2(data_type: &str) -> Box<Statements> {
+  let mut variable: Variable = get_variable("type");
+
+  let mut function: AnonymousFunction = Expression::from_token(Token::from_value(String::from("("), 1, 12));
+
+  function.data_type = Token::from_value(data_type.to_string(), 1, 16);
+
+  function.body = Block::new_box_from_token(Token::from_value(String::from("{"), 1, data_type.len() + 20));
+
+  variable.value = Some(Box::new(Expressions::ANONYMOUSFUNCTION(function)));
+
+  Box::new(Statements::VARIABLE(variable))
+}
+
+#[cfg(test)]
+fn arguments_anonymous_function_1() -> Box<Statements> {
+  let mut variable: Variable = get_variable("add");
+
+  let mut function: AnonymousFunction = Expression::from_token(Token::from_value(String::from("function"), 1, 11));
+
+  let mut x_arg = Argument::from_token(lexer::get_identifier("x", 1, 21));
+  x_arg.data_type = Token::from_value(String::from("number"), 1, 24);
+
+  function.arguments.push(Box::new(Expressions::ARGUMENT(x_arg)));
+
+  let mut y_arg = Argument::from_token(lexer::get_identifier("y", 1, 32));
+  y_arg.data_type = Token::from_value(String::from("number"), 1, 35);
+
+  function.arguments.push(Box::new(Expressions::ARGUMENT(y_arg)));
+
+  function.data_type = Token::from_value(String::from("number"), 1, 44);
+  function.body = Block::new_box_from_token(Token::from_value(String::from("{"), 1, 51));
+
+  variable.value = Some(Box::new(Expressions::ANONYMOUSFUNCTION(function)));
+
+  Box::new(Statements::VARIABLE(variable))
+}
+
+#[cfg(test)]
+fn arguments_anonymous_function_2() -> Box<Statements> {
+  let mut variable: Variable = get_variable("add");
+
+  let mut function: AnonymousFunction = Expression::from_token(Token::from_value(String::from("("), 1, 11));
+
+  let mut x_arg = Argument::from_token(lexer::get_identifier("x", 1, 12));
+  x_arg.data_type = Token::from_value(String::from("number"), 1, 15);
+
+  function.arguments.push(Box::new(Expressions::ARGUMENT(x_arg)));
+
+  let mut y_arg = Argument::from_token(lexer::get_identifier("y", 1, 23));
+  y_arg.data_type = Token::from_value(String::from("number"), 1, 26);
+
+  function.arguments.push(Box::new(Expressions::ARGUMENT(y_arg)));
+
+  function.data_type = Token::from_value(String::from("number"), 1, 35);
+  function.body = Block::new_box_from_token(Token::from_value(String::from("{"), 1, 45));
+
+  variable.value = Some(Box::new(Expressions::ANONYMOUSFUNCTION(function)));
+
+  Box::new(Statements::VARIABLE(variable))
+}
+
 #[test]
 fn parser_functions() {
-  let mut statements: Vec<Function> = Vec::new();
+  test_function("function empty() {}", empty_function());
 
-  // Empty function
-  statements.push(Statement::new());
+  test_function("function type(): string {}", type_function("string"));
+  test_function("function type(): number {}", type_function("number"));
+  test_function("function type(): boolean {}", type_function("boolean"));
+  test_function("function type(): void {}", type_function("void"));
 
-  statements[0].token = Token::from_value(String::from("function"), 1, 1);
-  statements[0].name = Identifier::new_box_from_token(lexer::get_identifier("empty", 1, 10));
-  statements[0].body = Block::new_box_from_token(Token::from_value(String::from("{"), 1, 18));
+  test_function("function add(x: number, y: number): number {}", arguments_function());
 
-  test_function("function empty() {}", get_box(statements[0].clone()));
+  // Anonymous functions.
+  test_function("let empty = function () {};", empty_anonymous_function_1());
+  test_function("let empty = () => {};", empty_anonymous_function_2());
+
+  test_function("let type = function (): string {};", type_anonymous_function_1("string"));
+  test_function("let type = function (): number {};", type_anonymous_function_1("number"));
+  test_function("let type = function (): boolean {};", type_anonymous_function_1("boolean"));
+  test_function("let type = function (): void {};", type_anonymous_function_1("void"));
+
+  test_function("let type = (): string => {};", type_anonymous_function_2("string"));
+  test_function("let type = (): number => {};", type_anonymous_function_2("number"));
+  test_function("let type = (): boolean => {};", type_anonymous_function_2("boolean"));
+  test_function("let type = (): void => {};", type_anonymous_function_2("void"));
+
+  test_function("let add = function (x: number, y: number): number {};", arguments_anonymous_function_1());
+  test_function("let add = (x: number, y: number): number => {};", arguments_anonymous_function_2());
 }
